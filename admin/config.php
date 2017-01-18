93729aaa4bd12a8d491f4876f33517e03dc82d27
Aaron <aaronsaban98@gmail.com>
2017-01-18 04:06:51 -0300

    0.1
    Header creado

---
+++ admin/config.php
@@ -0,0 +1,22 @@
+<?php 
+
+define('RUTA', 'http://localhost/BlogTest_PHP/');
+
+$bd_config = array(
+	'basedatos' => 'blog_php',
+	'usuario' => 'root',
+	'pass' => 'admin'
+);
+
+$blog_config = array(
+	'post_por_pagina' => '2',
+	'carpeta_imagens' => 'img/'
+);
+
+$blog_admin = array(
+	'usuario' => 'admin',
+	'password' => '1234'
+);
+
+
+ ?>
